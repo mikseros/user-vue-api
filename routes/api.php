9e7c4724da5bbe015830b67f99182431285628f2
mikseros <mmadynski@gmail.com>
2022-01-27 13:52:06 +0100

    Last two Routes (DELETE & UPDATE)

--- routes/api.php
+++ routes/api.php
@@ -72,3 +72,54 @@ Route::get('/users/all', function () {
         ]
     ];
 });
+
+// GET A SINGLE USER (BY ID)
+Route::get('/users/{id}', function (Request $request, $id) {
+    $user = User::find($id);
+
+    if (empty($user)) {
+        return [
+            "success" => false,
+            "response" => [
+                "error" => "No user found"
+            ]
+        ];
+    }
+
+    return [
+        "success" => true,
+        "response" => [
+            "user" => $user
+        ]
+    ];
+});
+
+// DELETE USER
+Route::delete('/users/delete/{id}', function (Request $request, $id) {
+    $user = User::find($id);
+
+    if (empty($user)) {
+        $success = false;
+        $response = ["error" => "User could not be deleted"];
+    } else {
+        $success = $user->delete();
+        $response = ["message" => "User deleted!"];
+    }
+
+    return ["success" => $success, "response" => $response];
+});
+
+// UPDATE USER
+Route::put('/users/update/{id}', function (Request $request, $id) {
+    $data = $request->all();
+
+    $user = User::find($id);
+
+    foreach ($data as $key => $value) {
+        $user->{$key} = $value;
+    }
+
+    $result = $user->save();
+
+    return ["success" => $result, "response" => ["user" => $user]];
+});
