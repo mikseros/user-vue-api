bf30a3a5c5bcf676b3e4c910bd5ce4e1fb52d652
mikseros <mmadynski@gmail.com>
2022-01-27 21:36:58 +0100

    App Header and some styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="dashboard">
+    <Header />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from '@/components/Header';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  --primary: #8A4CFC;
+  --primary-alt: #702fe7;
+  --light: #EEEEEE;
+  --light-alt: #F8F8F8;
+  --grey: #888888;
+  --dark: #131A26;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira-sans", sans-serif;
+}
+
+body {
+  background-color: var(--light);
+}
+
+main {
+  width: 100vw;
+  overflow: hidden;
+}
+
+input, button {
+  appearance: none;
+  outline: none;
+  border: none;
+  background: none;
+}
+
+.button {
+  display: inline-block;
+  padding: 8px 16px;
+  background-color: var(--primary);
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 700;
+  text-transform: uppercase;
+  cursor: pointer;
+  border-radius: 6px;
+  transition: 0.4s;
+}
+.button:hover {
+  background-color: var(--primary-alt);
+}
+
+.button.button-outline {
+  background-color: transparent;
+  border: 2px solid var(--primary);
+  padding: 6px 14px;
+  color: var(--primary);
+}
+
+.button.button-outline:hover {
+  color: #FFF;
+  background-color: var(--primary);
+}
+
+.button.button-small {
+  padding: 4px 8px;
+  font-size: 18px;
+  font-weight: 600;
+}
+
+.button.button-alert {
+  background-color: crimson;
+}
+
+.button-group {
+  display: flex;
+  margin: 0 -8px;
+}
+
+.button-group.group-end {
+  justify-content: flex-end;
+}
+
+.button-group .button {
+  margin: 0px 8px;
 }
 </style>
