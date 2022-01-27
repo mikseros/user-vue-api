10aa2803bdeff0fb8417329c070a707b22370a6e
mikseros <mmadynski@gmail.com>
2022-01-27 12:02:51 +0100

    First two routes. Create User & Get All Users

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,59 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//CREATE A USER
+Route::post('/users/create', function (Request $request) {
+    $data = $request->all();
+
+    if (!User::where('email', '=', $data['email'])->exists()) {
+        $user = User::create([
+            "name" => $data["name"],
+            "email" => $data["email"],
+            "password" => Hash::make($data["password"])
+        ]);
+
+        if (empty($user->id)) {
+            return [
+                "success" => false,
+                "response" => [
+                    "error" => "An unexpected error has occured"
+                ]
+            ];
+        } else {
+            return [
+                "success" => true,
+                "response" => [
+                    "user" => $user
+                ]
+            ];
+        }
+    } else {
+        return [
+            "success" => false,
+            "response" => [
+                "error" => "The user already exists"
+            ]
+        ];
+    }
+});
+
+// GET ALL USERS
+Route::get('/users/all', function () {
+    $users = User::all();
+
+    if (empty($users)) {
+        return [
+            "success" => false,
+            "response" => [
+                "error" => "No users found"
+            ]
+        ];
+    }
+
+    return [
+        "success" => true,
+        "response" => [
+            "users" => $users
+        ]
+    ];
 });
